3f2d30c0dfda4030902cafc425ce63e323f0b0fb
Noob-boyy <91830449+Noob-boyy@users.noreply.github.com>
2021-10-03 18:04:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 
 class Config(object):
-    SESSION_NAME = os.environ.get("SESSION_NAME")
-    API_ID = int(os.environ.get("API_ID", 12345678))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    SESSION_NAME = os.environ.get("SESSION_NAME", desitorrent bot))
+    API_ID = int(os.environ.get("API_ID", 467260))
+    API_HASH = os.environ.get("API_HASH", dd2ecf16ec56ffa8102d36d8b74328ad))
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", 2026125936:AAEKV_uG9CjUbAbL1dMr5WCOc00PZEd57LQ))
     MAX_INLINE_RESULTS = int(os.environ.get("MAX_INLINE_RESULTS", 50))
